df8aa413a253df7122b6d330950cf3775867f870
Aalekh Agarwal <74832537+Aalekh-lang@users.noreply.github.com>
2020-11-23 02:29:51 +0800

    Initial Commit

--- restfulapicrud/settings.py
+++ restfulapicrud/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,4 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+django_heroku.settings(locals())
