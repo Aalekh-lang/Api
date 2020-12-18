9fcd4a527d6847ec19f59822ec1cb5cf2a527e29
Aalekh Agarwal <74832537+Aalekh-lang@users.noreply.github.com>
2020-12-19 03:57:57 +0800

    Migration Changes

--- retailerapi/migrations/0001_initial.py
+++ retailerapi/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='ProductcodePriceAPI',
+            name='productcodepriceapi',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('BR_Code', models.CharField(max_length=20)),
