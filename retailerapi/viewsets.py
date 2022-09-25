3cab3bff474e9982e7c39bb868b8ef994cef99ef
Aalekh Agarwal <74832537+Aalekh-lang@users.noreply.github.com>
2022-09-26 02:49:27 +0800

    Adding the Update URL

--- retailerapi/viewsets.py
+++ retailerapi/viewsets.py
@@ -2,6 +2,11 @@
 from rest_framework import viewsets
 from . import models
 from . import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http.response import JsonResponse
+from django.db import  transaction
+import psycopg2
 
 class ProductViewset(viewsets.ModelViewSet):
 
@@ -45,3 +50,22 @@ class ProductViewsetPaymentMode(viewsets.ModelViewSet):
 	 	queryset = models.productrevenuedeatils_paymentmode.objects.filter(BR_Code=branchname,Retailer_ID=Retailerid)
 	 	#filter(Branch=branchname)
 	 	return queryset
+
+
+class ProductQuantitySerializer_Update(viewsets.ModelViewSet):
+	
+	serializer_class = serializers.ProductQuantitySerializerUpdate
+	queryset = models.productcodepriceapi.objects.all()
+	def get_queryset(self):
+		branchname = self.request.query_params.get('BR_Code', None)
+		Retailerid = self.request.query_params.get('Retailerid', None) 
+		Productid = self.request.query_params.get('Productid', None)
+		balancequantity = self.request.query_params.get('BalanceQuantity', None)
+		queryset = models.productcodepriceapi.objects.filter(BR_Code=branchname,Retailer_ID=Retailerid,P_Code=Productid)#.select_for_update().get() #update(BR_Code="THN1")
+		#Connection
+		conn = psycopg2.connect("host=ec2-34-193-44-192.compute-1.amazonaws.com dbname=d9ico489bsvs8l user=kybxasjbluxvel password=3d48454bfa4eb31d09f583cdac569004dd40d380bfdd22a0485a2c4adc84c742")
+		cur = conn.cursor()
+		sql = '''Update retailerapi_productcodepriceapi set  "BalanceQuantity" = '%s' where "BR_Code" = '%s' and "Retailer_ID" = %s and "P_Code" = '%s' '''
+		cur.execute(sql % (balancequantity,branchname ,Retailerid ,Productid))
+		conn.commit()
+		return queryset
